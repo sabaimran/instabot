d9b9bdf826f7c42f64c180b89c8268fc37bda253
Saba <narmiabas@gmail.com>
2021-12-12 16:35:09 -0500

    Drive settings through config file, automatically wait 5 seconds

--- instabot.py
+++ instabot.py
@@ -1,8 +1,7 @@
 from time import sleep
-
 from selenium import webdriver
-
 import yaml
+import argparse
 
 browser = None
 
@@ -18,28 +17,44 @@ def login_flow(username: str, password: str):
     login_button.click()
     sleep(5)
 
-def load_config():
+def load_config(config_path: str):
     # Load the config file
-    global config
-    with open('config.yml', 'r') as config_file:
-        config = yaml.safe_load(config_file)
+    with open(config_path, 'r') as config_file:
+        return yaml.safe_load(config_file)
 
 def open_browser():
     # Setup and open the Firefox browser
     global browser
     browser = webdriver.Firefox()
+    browser.implicitly_wait(5)
     browser.get('https://www.instagram.com/')
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Instagram Bot')
+    parser.add_argument('-c', '--config', help='Config file', required=False)
+    return parser.parse_args()
+
+def select_new_post_button():
+    # Select the new post button
+    new_post_button = browser.find_element_by_css_selector("button[class='wpO6b ZQScA ']")
+    new_post_button.click()
 
 if __name__ == '__main__':
 
+    args = parse_arguments()
+
     # load settings
-    config = load_config()
+    config = load_config(args.config)
 
     # open browser
     open_browser()
 
     # login to Instagram
     login_flow(config['username'], config['password'])
+
+    sleep(5)
+
+    select_new_post_button()
 
     sleep(5)
 
