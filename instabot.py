6d1f227c70412ce2cf6e7fd1febaa38639cdeb98
Saba <narmiabas@gmail.com>
2021-12-12 16:15:54 -0500

    Clean up methods and add comments for login flow

--- instabot.py
+++ instabot.py
@@ -4,36 +4,42 @@ from selenium import webdriver
 
 import yaml
 
-config = {}
+browser = None
 
-def login_flow():
-    # login
+def login_flow(username: str, password: str):
+    # Login to Instagram
     username_input = browser.find_element_by_css_selector("input[name='username']")
     password_input = browser.find_element_by_css_selector("input[name='password']")
 
-    username_input.send_keys(config['username'])
-    password_input.send_keys(config['password'])
+    username_input.send_keys(username)
+    password_input.send_keys(password)
 
     login_button = browser.find_element_by_css_selector("button[type='submit']")
     login_button.click()
     sleep(5)
 
 def load_config():
+    # Load the config file
     global config
     with open('config.yml', 'r') as config_file:
-        config = yaml.load(config_file)
+        config = yaml.safe_load(config_file)
+
+def open_browser():
+    # Setup and open the Firefox browser
+    global browser
+    browser = webdriver.Firefox()
+    browser.get('https://www.instagram.com/')
 
 if __name__ == '__main__':
 
     # load settings
+    config = load_config()
 
     # open browser
-    browser = webdriver.Firefox()
-    browser.implicitly_wait(5)
+    open_browser()
 
-    browser.get('https://www.instagram.com/')
-
-    login_flow()
+    # login to Instagram
+    login_flow(config['username'], config['password'])
 
     sleep(5)
 
